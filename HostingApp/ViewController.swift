b1be94899521fa33a397d2ff2215278b2c23593c
Shashank Patel <patel.shashank2001@gmail.com>
2016-03-29 20:24:30 +0530

    Proof of concept

---
+++ HostingApp/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  TransliteratingKeyboard
+//
+//  Created by Shashank Patel on 3/25/16.
+//  Copyright (c) 2014 Shashank Patel. All rights reserved.
+//
+
+import UIKit
+
+class HostingAppViewController: UIViewController {
+    
+    @IBOutlet var stats: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow"), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardDidHide"), name: UIKeyboardDidHideNotification, object: nil)
+        //NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillChangeFrame:"), name: UIKeyboardWillChangeFrameNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardDidChangeFrame:"), name: UIKeyboardDidChangeFrameNotification, object: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func dismiss() {
+        for view in self.view.subviews {
+            if let inputView = view as? UITextField {
+                inputView.resignFirstResponder()
+            }
+        }
+    }
+    
+    var startTime: NSTimeInterval?
+    var firstHeightTime: NSTimeInterval?
+    var secondHeightTime: NSTimeInterval?
+    var referenceHeight: CGFloat = 216
+    
+    func keyboardWillShow() {
+        if startTime == nil {
+            startTime = CACurrentMediaTime()
+        }
+    }
+    
+    func keyboardDidHide() {
+        startTime = nil
+        firstHeightTime = nil
+        secondHeightTime = nil
+        
+        self.stats?.text = "(Waiting for keyboard...)"
+    }
+    
+    func keyboardDidChangeFrame(notification: NSNotification) {
+        //let frameBegin: CGRect! = notification.userInfo?[UIKeyboardFrameBeginUserInfoKey]?.CGRectValue
+        let frameEnd: CGRect! = notification.userInfo?[UIKeyboardFrameEndUserInfoKey]?.CGRectValue
+        
+        if frameEnd.height == referenceHeight {
+            if firstHeightTime == nil {
+                firstHeightTime = CACurrentMediaTime()
+                
+                if let startTime = self.startTime {
+                    if let firstHeightTime = self.firstHeightTime {
+                        let formatString = NSString(format: "First: %.2f, Total: %.2f", (firstHeightTime - startTime), (firstHeightTime - startTime))
+                        self.stats?.text = formatString as String
+                    }
+                }
+            }
+        }
+        else if frameEnd.height != 0 {
+            if secondHeightTime == nil {
+                secondHeightTime = CACurrentMediaTime()
+
+                if let startTime = self.startTime {
+                    if let firstHeightTime = self.firstHeightTime {
+                        if let secondHeightTime = self.secondHeightTime {
+                            let formatString = NSString(format: "First: %.2f, Second: %.2f, Total: %.2f", (firstHeightTime - startTime), (secondHeightTime - firstHeightTime), (secondHeightTime - startTime))
+                            self.stats?.text = formatString as String
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
